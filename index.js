f94f6ece7d0cb8c421093ce91fb872a86faaa07e
Bernabe19 <bernasanchezsos4@gmail.com>
2022-08-12 19:43:54 +0200

    act

--- index.js
+++ index.js
@@ -3,45 +3,43 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 let bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-const dns = require('dns');
+const isUrl = require('is-url');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let count = 0;
+const urls = {};
+
+
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended:false}));
+
+
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.post('/api/shorturl', function(req,res){
+  var cadena = req.body.url;
+  if(!isUrl(cadena)){
+    res.json({error: "invalid url"});
+    return;
+  }
+    count+=1;
+    urls[count] = cadena;
+    res.json({original_url : cadena,short_url : count});
+});
+
+app.get('/api/shorturl/:ur', function(req, res) {
+  const short = req.params.ur;
+  const url = urls[short];  
+    res.redirect(url);
+  
+});
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
-});
-
-var cadena;
-var num = 0;
-app.post('/api/shorturl', function(req,res){
-  num = Math.floor(Math.random() * 1000);
-  cadena = req.body.url;
-  let correct = true;
-  dns.lookup(cadena,function(err){
-    if(err) correct = false;
-  });
-
-  if(correct == null || correct == false){
-    res.json({error:"Invalid url"});
-  }else{
-    res.json({"original_url":cadena,"short_url":num});
-  }
-
-  
-});
-app.get('/api/shorturl/:short_url', function(req, res) {
-  var ruta = req.headers.short_url;
-  if(ruta == num){
-    res.redirect({cadena});
-  }
- 
 });
 
 
