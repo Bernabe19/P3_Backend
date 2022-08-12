b668cadff0259b0fd6023576b2b801cb870fce71
Bernabe19 <bernasanchezsos4@gmail.com>
2022-08-12 18:34:53 +0200

    end

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +18,33 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+var cadena;
+var num = 0;
+app.post('/api/shorturl', function(req,res){
+  num = Math.floor(Math.random() * 1000);
+  cadena = req.body.url;
+  let correct = true;
+  dns.lookup(cadena,function(err){
+    if(err) correct = false;
+  });
+
+  if(correct == null || correct == false){
+    res.json({error:"Invalid url"});
+  }else{
+    res.json({"original_url":cadena,"short_url":num});
+  }
+
+  
 });
+app.get('/api/shorturl/:short_url', function(req, res) {
+  var ruta = req.headers.short_url;
+  if(ruta == num){
+    res.redirect({cadena});
+  }
+ 
+});
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
